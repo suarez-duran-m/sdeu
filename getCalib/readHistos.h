a67e0d055467d09aff795a36ab3748833e7f5d12
Mauricio <mauromsd@gmail.com>
2021-03-19 10:32:13 +0100

    Version with classes and MSD's to get the VEM position

--- getCalib/readHistos.h
+++ getCalib/readHistos.h
@@ -5,6 +5,7 @@
 //#include <TH1.h>
 class TH1;
 class TF1;
+class TGraphErrors;
 class TGH1F;
 class TList;
 
@@ -14,13 +15,17 @@ class readHistos {
   public:
 		readHistos();
     unsigned int binVm;
-		TF1 *gMean;
-    unsigned int getBinVem(TH1F *hist);
+		TF1 *gaussFit;
+    unsigned int getBinVem(TH1F &hist);
 
-		double getFitVem(TH1F *hist, bool ifch);
+		double getFitVem(TH1F &hist, const bool ifch);
+		TGraphErrors *getFullFit(TH1F &hist, const bool ifch, const double frac, const int fstbinFit);
 
 		double area;
 		double peak;
+
+	//private:
+		//double fitFunction( double *x, double *par);
 };
 
 #endif
