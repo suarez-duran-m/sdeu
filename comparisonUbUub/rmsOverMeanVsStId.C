06315f5484f698e5ee08798bce31c523c21c60e1
suarez-duran-m <mauromsd@gmail.com>
2021-11-04 14:40:13 +0100

    Minor changes to prodece the results in gapnote/accuracyQpk/

--- comparisonUbUub/rmsOverMeanVsStId.C
+++ comparisonUbUub/rmsOverMeanVsStId.C
@@ -33,12 +33,12 @@ TH1D *getRelQpk( TH1D *qpkVals, vector<double> &relQpkAllPmts, int pmt,//) {
   double mean = qpkVals->GetMean();
   TString strMean;
   strMean.Form("%.4f", mean);
-  int nbins = 2000;
+  int nbins = 200;
   double frtBin = 0.;
   double lstBin = 2.;
   double qpk = 0.;
   double relQpk = 0.;
-  double relQpk2Bin = 1000.;
+  double relQpk2Bin = nbins/2.;
   int bin = 0;
   int nCnts = 0;
   bool doCnts = false;
@@ -73,7 +73,7 @@ TH1D *getRelQpk( TH1D *qpkVals, vector<double> &relQpkAllPmts, int pmt,//) {
       }
       if ( relQpk == 0 )
         continue;
-      bin = relQpk2Bin*(relQpk+5e-4);
+      bin = relQpk2Bin*(relQpk+(lstBin/nbins));
       nCnts = qpkVals->GetBinContent(qpk_i);
       nCnts += retRelQpk->GetBinContent( bin-1 );
       relQpkAllPmts[bin] += nCnts;
@@ -83,7 +83,7 @@ TH1D *getRelQpk( TH1D *qpkVals, vector<double> &relQpkAllPmts, int pmt,//) {
     }
     else {
       relQpk = qpk/mean;
-      bin = relQpk2Bin*(relQpk+5e-4); 
+      bin = relQpk2Bin*(relQpk+(lstBin/nbins));
       nCnts = qpkVals->GetBinContent(qpk_i);
       relQpkAllPmts[bin] += nCnts;
       // Add the entries for same qpk/mean bins, before the
@@ -203,29 +203,28 @@ IfCtionAndLnForSt ifCationSt(vector<vector<double>> listCtionSts,
 }
 
 
-double getmean( vector<double> arr ) {
-  int nb = arr.size();
+double getmean( TH1D *arr ) {
+  int nb = arr->GetNbinsX();
   int goodVals = 0;
   double mean = 0.;
   for (int bin_i=0; bin_i<nb; bin_i++)
-    if ( arr[bin_i] > 0 ) {
-      mean += arr[bin_i];
-      goodVals++;
+    if ( arr->GetBinContent(bin_i) > 0 && arr->GetBinCenter(bin_i)<3.5) {
+      mean += arr->GetBinCenter(bin_i)*arr->GetBinContent(bin_i);
+      goodVals += arr->GetBinContent(bin_i);
     }
   return mean/goodVals;
 }
 
-double getrms( vector<double> arr, double meanarr ) {
-  int nb = arr.size();
+double getrms( TH1D *arr, double meanarr ) {
+  int nb = arr->GetNbinsX();
   double rms = 0.;
   int goodVals = 0;
   for (int bin_i=0; bin_i<nb; bin_i++)
-    if ( arr[bin_i]> 0 ) {
-      rms += (arr[bin_i]-meanarr)*(arr[bin_i]-meanarr);
-      goodVals++;
+    if ( arr->GetBinContent(bin_i) > 0 && arr->GetBinCenter(bin_i)<3.5 ) {
+      rms += arr->GetBinContent(bin_i)*arr->GetBinCenter(bin_i)*arr->GetBinCenter(bin_i);
+      goodVals += arr->GetBinContent(bin_i);
     }
-
-  return sqrt(rms/goodVals);
+  return sqrt(rms/goodVals-(meanarr*meanarr));
 }
 
 TH1D *getDistRelRMSqpk(bool ifUub) {
@@ -254,10 +253,10 @@ TH1D *getDistRelRMSqpk(bool ifUub) {
   vector < vector < double > > cautionStPmt3;
   cautionStPmt3 = fecthCtionData(3);
 
-  int nbins = 2000;
+  int nbins = 200;
   double frtBin = 0.;
   double lstBin = 2.;
-  TH1D *distRelQpkPmt1 = new TH1D(); 
+  TH1D *distRelQpkPmt1 = new TH1D();
   TH1D *distRelQpkPmt2 = new TH1D(); 
   TH1D *distRelQpkPmt3 = new TH1D(); 
   vector < double > relQpkPmts;
@@ -273,18 +272,21 @@ TH1D *getDistRelRMSqpk(bool ifUub) {
 
   double rmsRelQpkAllPmt = 0.;
   double rmsRelQpkPmt = 0.;
-  double farFromSigma = 1.1;
+  double farFromSigma = 1.3;
   int cutForMeanByPmt = 0;
 
+  //TH1D *diffMuRMS = new TH1D ("diffMuRMS","", 2000, -1, 1);
+
   for ( int etry=0; etry<stIds->GetEntries(); etry++ ) {
-    //if ( etry != 0 ) // 8 for st827; 12 for st833
-      //continue;
+    // 8 for st827; 12 for st833; 67 for 1746; 14 for 843
     stIds->GetEntry( etry );
-    //cout << etry << " " << stid << endl;
+    //if ( stid != 1878 )
+      //continue;
+            
     for ( int i=0; i<nbins; i++ )
       relQpkPmts.push_back(0.);
 
-    IfCtionAndLnForSt tmp = ifCationSt( cautionStPmt1, stid, ifUub);
+    IfCtionAndLnForSt tmp = ifCationSt( cautionStPmt1, stid, ifUub );
     isCtionSt = tmp.IfCtion;
     lnCationSt = tmp.lnForSt;
     distRelQpkPmt1 = getRelQpk( fetchQpkValsPmt1, relQpkPmts, 1, 
@@ -307,88 +309,126 @@ TH1D *getDistRelRMSqpk(bool ifUub) {
         distRelQpkPmts->SetBinContent( bin-1, relQpkPmts[bin] );
 
     rmsRelQpkAllPmt = farFromSigma*distRelQpkPmts->GetRMS();
+    
+    /*
+    TCanvas *c000 = canvasStyle("c000");
+    c000->cd();
+    distRelQpkPmts->Draw();
+    gPad->WaitPrimitive();
+    */
     cutForMeanByPmt = (ifUub) ? 500 : 50;
+
     rmsRelQpkPmt = distRelQpkPmt1->GetRMS();
     if ( rmsRelQpkPmt < rmsRelQpkAllPmt 
         && fetchQpkValsPmt1->GetMean() > cutForMeanByPmt )
       fillRelQpkOk(distRelQpkPmt1, distRelQpkOk);
+
     rmsRelQpkPmt = distRelQpkPmt2->GetRMS();
     if ( rmsRelQpkPmt < rmsRelQpkAllPmt 
         && fetchQpkValsPmt2->GetMean() > cutForMeanByPmt )
       fillRelQpkOk(distRelQpkPmt2, distRelQpkOk);
+
     rmsRelQpkPmt = distRelQpkPmt3->GetRMS();
     if ( rmsRelQpkPmt < rmsRelQpkAllPmt 
         && fetchQpkValsPmt3->GetMean() > cutForMeanByPmt )
       fillRelQpkOk(distRelQpkPmt3, distRelQpkOk);
+    
+    Int_t status = distRelQpkOk->Fit("gaus","","",distRelQpkOk->GetMean()-2.*distRelQpkOk->GetRMS(),
+        distRelQpkOk->GetMean()+2.*distRelQpkOk->GetRMS());
+   
+    if ( status == 0 )
+      rmsRelQpkAllPmt = 100.*distRelQpkOk->GetFunction("gaus")->GetParameter(2)/distRelQpkOk->GetFunction("gaus")->GetParameter(1);
 
-    rmsRelQpkAllPmt = 100.*distRelQpkOk->GetRMS()/distRelQpkOk->GetMean();
-    retDistRelRMSqpk->Fill( rmsRelQpkAllPmt );
-    if ( ifUub && rmsRelQpkAllPmt > 3.5 )
-      cout << "MSD stid " << stid << endl;
-
-    if ( ifUub ) {
+    if ( rmsRelQpkAllPmt > 0 )
+      retDistRelRMSqpk->Fill( rmsRelQpkAllPmt );
+    if ( !ifUub && rmsRelQpkAllPmt > 4.5 ) // && rmsRelQpkAllPmt < 2.9 )
+      cout << endl << endl
+        << "=============================" << endl
+        << "MSD stid " << stid 
+        << " ifUub " << ifUub
+        << " etry " << etry 
+        << " " << distRelQpkOk->GetRMS()
+        << " " << distRelQpkOk->GetMean() << endl << endl;
+    
+    if ( !ifUub ) {
       TLegend *leg;
       TString strTitle;
-      strTitle.Form("Station %d",stid);
+      strTitle.Form("Station %d UUB",stid);
+      //strTitle.Form("Station %d UB",stid);
+
       /*
       TCanvas *c0 = canvasStyle("c0");
       c0->cd();
-      
+        
       //distRelQpkOk->Fit("gaus","","",distRelQpkOk->GetMean()-distRelQpkOk->GetRMS(),
         //  distRelQpkOk->GetMean()+distRelQpkOk->GetRMS());
+      
+      //if ( distRelQpkOk->GetMean() > 0 )
+        //diffMuRMS->Fill( distRelQpkOk->GetFunction("gaus")->GetParameter(2) - distRelQpkOk->GetRMS() );
       //cout << "MSD gaus " << distRelQpkOk->GetFunction("gaus")->GetParameter(2) << endl;
       //cout << "MSD dist " << rmsRelQpkAllPmt << endl;
-
-      distRelQpkOk->GetXaxis()->SetRangeUser(0.8, 1.2);
-      //distRelQpkOk->GetXaxis()->SetRangeUser(0.85, 1.15);
+            
+      distRelQpkOk->SetStats(kFALSE);
+      distRelQpkOk->GetXaxis()->SetRangeUser(0.78, 1.26);
+      distRelQpkOk->GetXaxis()->SetRangeUser(0.9, 1.1); 
       distRelQpkOk->GetXaxis()->SetTitle("Q^{pk}_{i}/#LT Q^{pk}_{VEM}#GT_{PMT}");
       distRelQpkOk->GetYaxis()->SetTitle("Counts [au]");
       distRelQpkOk->SetLineColor(kMagenta-3);
       distRelQpkOk->SetTitle(strTitle);
       distRelQpkOk->Draw();
+      
       distRelQpkPmt1->SetLineColor(kBlack);
       distRelQpkPmt1->Draw("same");
+      
       distRelQpkPmt2->SetLineColor(kBlue);
       distRelQpkPmt2->Draw("same");
       distRelQpkPmt3->SetLineColor(kGreen+2);
       distRelQpkPmt3->Draw("same");
-
-      leg = new TLegend(0.8,0.5,0.9,0.6);
+      
+      leg = new TLegend(0.8,0.5,0.9,0.9);
       //leg->SetHeader("Filtered");
-      //strTitle.Form("%.5f", 
-        //  distRelQpkOk->GetFunction("gaus")->GetParameter(1));
-      //leg->AddEntry(distRelQpkOk,"#mu fit "+strTitle);
-      //strTitle.Form("%.5f",
-        //  distRelQpkOk->GetFunction("gaus")->GetParameter(2));
-      //leg->AddEntry(distRelQpkOk,"#sigma fit "+strTitle);
+      leg->AddEntry(distRelQpkOk->GetFunction("gaus"),"Gaus fit", "l");
+      strTitle.Form("%.5f", 
+          distRelQpkOk->GetFunction("gaus")->GetParameter(1));
+      leg->AddEntry(distRelQpkOk->GetFunction("gaus"),"#mu = "+strTitle, "");
+      strTitle.Form("%.5f",
+          distRelQpkOk->GetFunction("gaus")->GetParameter(2));
+      leg->AddEntry(distRelQpkOk->GetFunction("gaus"),"#sigma = "+strTitle, "");
+      leg->AddEntry(distRelQpkOk,"All PMTs", "l");
+      leg->AddEntry(distRelQpkPmt1, "PMT1", "l");
+      leg->AddEntry(distRelQpkPmt2, "PMT2", "l");
+      leg->AddEntry(distRelQpkPmt3, "PMT3", "l");
       leg->SetTextSize(0.03);
       leg->SetBorderSize(0);
       leg->SetFillStyle(0);
       leg->Draw();
       strTitle.Form("filteredSt%d",stid);
+      //strTitle.Form("filteredUbSt%d",stid);
       //strTitle.Form("nofilterSt%d",stid);
-      c0->Print("../plots/"+strTitle+".pdf");
-      //gPad->WaitPrimitive();
+      //c0->Print("../plots/"+strTitle+".pdf");
+      gPad->WaitPrimitive();
       */
-      /* 
+      /*
       TCanvas *c00 = canvasStyle("c00");
       c00->cd();
-      strTitle.Form("Station %d",stid);
+      strTitle.Form("Station %d UUB",stid);
       fetchQpkValsPmt1->SetStats(kFALSE);
       fetchQpkValsPmt1->SetTitle(strTitle);
       fetchQpkValsPmt1->SetLineColor(kRed);
       fetchQpkValsPmt1->GetXaxis()->SetTitle("Q^{pk}_{VEM}");
       fetchQpkValsPmt1->GetYaxis()->SetTitle("Counts [au]");
-      fetchQpkValsPmt1->GetXaxis()->SetRangeUser(1500, 1950);
-      fetchQpkValsPmt1->GetYaxis()->SetRangeUser(0, 850);
+      fetchQpkValsPmt1->GetXaxis()->SetRangeUser(1450, 1850);
+      //fetchQpkValsPmt1->GetXaxis()->SetRangeUser(145, 195);
+      fetchQpkValsPmt1->GetYaxis()->SetRangeUser(0, 45);
       fetchQpkValsPmt1->Draw();
       fetchQpkValsPmt2->SetLineColor(kBlue);
       fetchQpkValsPmt2->Draw("same");
       fetchQpkValsPmt3->SetLineColor(kGreen+2);
       fetchQpkValsPmt3->Draw("same");
       strTitle.Form("filteredPMTsSt%d",stid);
-      c00->Print("../plots/"+strTitle+".pdf");
-      //gPad->WaitPrimitive();
+      //strTitle.Form("filteredUbPMTsSt%d",stid);
+      //c00->Print("../plots/"+strTitle+".pdf");
+      gPad->WaitPrimitive();
       */
     }
     distRelQpkPmt1->Reset();
@@ -399,6 +439,17 @@ TH1D *getDistRelRMSqpk(bool ifUub) {
 
     relQpkPmts.clear();
   }
+  /*
+  if( ifUub ) {
+    TCanvas *c0 = canvasStyle("c0");
+    c0->cd();
+    diffMuRMS->GetYaxis()->SetTitle("Counts [au]");
+    diffMuRMS->GetXaxis()->SetTitle("(#mu - RMS) [au]");
+    diffMuRMS->GetXaxis()->SetRangeUser(-0.03, 0.04);
+    diffMuRMS->Draw();
+    c0->Print("../plots/diffMuRMS.pdf");
+  }
+  */
   return retDistRelRMSqpk; 
   stIds->Clear();
   f->Close();
@@ -408,7 +459,6 @@ void rmsOverMeanVsStId() {
 
   TPaveStats *ptstats;
   TLegend *leg;
-  TString strEntr;
   TString strMean;
   TString strRms;
  
@@ -428,27 +478,32 @@ void rmsOverMeanVsStId() {
   distRelRMSqpkUub->SetLineColor(kRed);
   distRelRMSqpkUub->SetLineWidth(2);
   distRelRMSqpkUub->GetYaxis()->SetTitle("Counts [au]");
-  distRelRMSqpkUub->GetXaxis()->SetTitle("#LT RMS/#LT Q^{pk}_{VEM} #GT #GT [%]");
+  distRelRMSqpkUub->GetXaxis()->SetTitle("#sigma/#mu [%]");
   distRelRMSqpkUub->GetXaxis()->SetRangeUser(0, 10);
+  distRelRMSqpkUub->GetYaxis()->SetRangeUser(0, 13.5);
+  distRelRMSqpkUub->SetFillStyle(3345);
+  distRelRMSqpkUub->SetFillColor(kRed);
   distRelRMSqpkUub->Draw();
 
   distRelRMSqpkUb->SetLineColor(kBlack);
   distRelRMSqpkUb->SetLineWidth(2);
+  distRelRMSqpkUb->SetFillStyle(3354);
+  distRelRMSqpkUb->SetFillColor(kBlack);
   distRelRMSqpkUb->Draw("same");  
 
   leg = new TLegend(0.7,0.65,0.9,0.95);
   
-  strMean.Form("%.3f", distRelRMSqpkUb->GetMean());
-  strRms.Form("%.3f", distRelRMSqpkUb->GetRMS());
+  strMean.Form("%.3f", getmean(distRelRMSqpkUb));
+  strRms.Form("%.3f", getrms(distRelRMSqpkUb, getmean(distRelRMSqpkUb)));
   leg->AddEntry(distRelRMSqpkUb, "UB", "l" );
-  leg->AddEntry(distRelRMSqpkUb, "MEAN: "+strMean, "");
-  leg->AddEntry(distRelRMSqpkUb, "RMS: "+strRms, "");
+  leg->AddEntry(distRelRMSqpkUb, "MEAN (<3.5 %): "+strMean, "");
+  leg->AddEntry(distRelRMSqpkUb, "RMS (<3.5 %): "+strRms, "");
   
-  strMean.Form("%.3f", distRelRMSqpkUub->GetMean());
-  strRms.Form("%.3f", distRelRMSqpkUub->GetRMS());
+  strMean.Form("%.3f", getmean(distRelRMSqpkUub));
+  strRms.Form("%.3f", getrms(distRelRMSqpkUub, getmean(distRelRMSqpkUub)));
   leg->AddEntry(distRelRMSqpkUub, "UUB", "l" );
-  leg->AddEntry(distRelRMSqpkUub, "MEAN: "+strMean, "");
-  leg->AddEntry(distRelRMSqpkUub, "RMS: "+strRms, "");
+  leg->AddEntry(distRelRMSqpkUub, "MEAN (<3.5 %): "+strMean, "");
+  leg->AddEntry(distRelRMSqpkUub, "RMS (<3.5 %): "+strRms, "");
   leg->SetTextSize(0.03);
   leg->SetBorderSize(0);
   leg->SetFillStyle(0);
