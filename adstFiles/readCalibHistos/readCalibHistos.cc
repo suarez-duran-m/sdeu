75167ab10d01c1a95c357be6fbb81ff52dbda368
suarez-duran-m <mauromsd@gmail.com>
2022-03-31 17:43:20 +0200

    Changes to print Stations in agreement with coincident histograms

--- adstFiles/readCalibHistos/readCalibHistos.cc
+++ adstFiles/readCalibHistos/readCalibHistos.cc
@@ -23,22 +23,22 @@ using namespace std;
 
 int main ( int argc, char** argv) {
   if ( argc < 2 ) { cout << endl
-      << "Usage: " << argv[0] << " <adst_file> <output> " << endl
-      << "<adst_file>: ADST File to read" << endl
-      << "<st_time_list>: ASCII with St. IDs and GPS time to read" 
+      << "Usage: " << argv[0] << " <st_time_list> <adst_file>" << endl
+      << "<st_time_list>: ASCII with St. IDs and GPS time to read" << endl
+      << "<adst_file>: ADST File to read"
       << endl;
     exit(0);
   }
   cout << endl << endl;
   // Reading the ASCII file
-  const char* stationsFileName = argv[2];
+  const char* stationsFileName = argv[1];
   ifstream stationsFile(stationsFileName, ios::in);
   if (!stationsFile.is_open()){
     cout << "Could not open file: " << stationsFileName << endl;
     exit(0);
   }
   vector< unsigned int > st2read;
-  vector< double > gps2read;
+  vector< int > gps2read;
   unsigned int st = 0;
   double gps = 0.;
   double utc = 0.;
@@ -53,34 +53,47 @@ int main ( int argc, char** argv) {
   // Creating file to store charge histograms
   fstream file;
   string outFileHistName = "nan";
+  bool readSt = false;
+  int nEvtFound = 0;
+  file.open("events.dat", ios_base::out);
+  bool stFound = false;
 
   // Reading ADST files
-  for ( int adst_i = 1; adst_i<argc-1; adst_i++ ) {
+  for ( int adst_i = 2; adst_i<argc; adst_i++ ) {
     RecEventFile inputFile(argv[adst_i]);
     RecEvent *theRecEvent = new RecEvent();
     inputFile.SetBuffers(&theRecEvent);
     cout << "Opened " << argv[adst_i]
       << " with " << inputFile.GetNEvents()
       << " events " << endl;
-
-    bool readSt = false;
     // Reading events    
     while ( inputFile.ReadNextEvent() == RecEventFile::eSuccess ) {
       const auto &sdEvent = theRecEvent->GetSDEvent();
-      int nCand = sdEvent.GetNumberOfCandidates();      
+      int nCand = sdEvent.GetNumberOfCandidates();
+      stFound = false;
       // Reading station in the event
       for ( int st_i=0; st_i<nCand; st_i++ ) {
         readSt = false;
+        //cout << sdEvent.GetStation(st_i)->GetId() << endl;
+        if ( sdEvent.GetStation(st_i)->GetId() == 1205 )
+          stFound = true;
         // Looking if there is a desired station
         for ( int st2r_i=0; st2r_i<st2read.size(); st2r_i++ ) {
-          if ( sdEvent.GetStation(st_i)->GetId() == st2read[st2r_i] ) {
-            if ( sdEvent.GetGPSSecond() > (gps2read[st2r_i] - 1) &&
-                sdEvent.GetGPSSecond() < (gps2read[st2r_i] + 1) )
-                readSt = true;
+          //cout << sdEvent.GetGPSSecond() << " " << (int)gps2read[st2r_i] << endl;
+          if ( sdEvent.GetGPSSecond() == gps2read[st2r_i] ) {
+            if ( sdEvent.GetStation(st_i)->GetId() == st2read[st2r_i] ) {
+              readSt = true;
+              file << st2read[st2r_i] << " "
+                << sdEvent.GetEventId() << " "
+                << sdEvent.GetGPSSecond() << endl;
+            }
           }
         }
         if ( !readSt )
           continue;
+        nEvtFound++;
+        //cout << sdEvent.GetEventId() << endl;
+        /*
         // Reading and storing Charge histogram
         if ( sdEvent.HasCalibrationHistograms() ) {
           // Fetching events traces
@@ -89,7 +102,7 @@ int main ( int argc, char** argv) {
           for (vector<Traces>::const_iterator trIter = traces.begin();
               trIter != traces.end(); ++trIter) {
             if ( trIter->GetType() != eTotalTrace )
-              continue;
+              continue; 
             // Excluding SSD-PMT and SPMT
             if ( trIter->GetPMTId() == 4 || trIter->GetPMTId() == 5 )
               continue;
@@ -105,8 +118,12 @@ int main ( int argc, char** argv) {
             file.close();
           }
         }
+        */
       }
+      cout << "MSD " << sdEvent.GetEventId() << " " << stFound << endl;
     }
   }
+  cout << nEvtFound << endl;
+  file.close();
   return 0;
 }
